6474bf07144455d8043ee00a0b0cd3942f262a5c
Flame-eth <theflamepen@gmail.com>
2024-01-27 16:10:56 +0100

    completed

---
+++ data/monthly-sales.ts
@@ -0,0 +1,14 @@
+export const monthlySales = [
+  { id: 1, month: "2023-01", totalSales: 3660 },
+  { id: 2, month: "2023-02", totalSales: 5957 },
+  { id: 3, month: "2023-03", totalSales: 9028 },
+  { id: 4, month: "2023-04", totalSales: 1081 },
+  { id: 5, month: "2023-05", totalSales: 8062 },
+  { id: 6, month: "2023-06", totalSales: 7178 },
+  { id: 7, month: "2023-07", totalSales: 4313 },
+  { id: 8, month: "2023-08", totalSales: 4483 },
+  { id: 9, month: "2023-09", totalSales: 3280 },
+  { id: 10, month: "2023-10", totalSales: 5718 },
+  { id: 11, month: "2023-11", totalSales: 1891 },
+  { id: 12, month: "2023-12", totalSales: 4488 },
+];
---
+++ data/platforms.ts
@@ -0,0 +1,27 @@
+export const platforms = [
+  {
+    name: "Book Bazaar",
+    amount: 372595,
+    previousAmount: 888625,
+  },
+  {
+    name: "Artisan Aisle",
+    amount: 873319,
+    previousAmount: 393851,
+  },
+  {
+    name: "Toy Troop",
+    amount: 612955,
+    previousAmount: 698985,
+  },
+  {
+    name: "XStore",
+    amount: 234948,
+    previousAmount: 540550,
+  },
+  {
+    name: "Tabular Razar",
+    amount: 573818,
+    previousAmount: 774049,
+  },
+];
